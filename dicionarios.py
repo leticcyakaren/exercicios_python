a2acd1dac0102fed5e1f0dcd8bc06749dc85df67
leticcyakaren <leticyakarensf@gmail.com>
2026-06-17 19:59:37 -0300

    Create dicionarios.py

---
+++ dicionarios.py
@@ -0,0 +1,56 @@
+"perfil"
+#perfil = { 
+#"nome":"letty",
+#"idade":"21",
+#"cidade":"paulista"}
+
+#print(perfil["nome"])
+#print(perfil["idade"])
+#print(perfil["cidade"])
+
+
+
+
+"livro"
+#livro = {
+#"autor": "machado de assis",
+#"titulo" : "sitio do picapau amarelo",
+##"ano":"1223"}
+##print(livro["ano"])
+#print(livro["autor"])
+#print(livro["titulo"])
+
+
+
+"produto/alteração"
+#produto = {
+#"nome":"batata",
+#"preco": 2.00 ,
+#"estoque":100}
+#produto["estoque"] = 1500
+#print(produto)
+
+
+"adicionar chave"
+#aluno = {
+#"nome":"ana",
+#"idade":"19"}
+#aluno["curso"]="ads"
+#print(aluno)
+
+
+
+"sistema de cadastro simples"
+#usuario = {
+#"nome":"",
+#"idade":"",
+#"curso":""}
+
+#nome=input("digite seu nome: ")
+#usuario["nome"] =nome
+#idade = int(input("digite sua idade: "))
+#usuario["idade"]=idade
+#curso= input("digite seu curso: ")
+#usuario["curso"]=curso
+
+#print(usuario)
